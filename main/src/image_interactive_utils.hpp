ec4a09b83874b880c552557cb16903fb9d6e577a
JaderoChan <c_dl_cn@outlook.com>
2026-04-02 03:48:43 +0800

    remove subproject `main`

--- main/src/image_interactive_utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <opencv2/opencv.hpp>
-
-cv::Point selectImagePoint(const cv::Mat& image, int originX = 0, int originY = 0,
-    const cv::Scalar& hintColor = cv::Scalar(0, 0, 255));
-
-void cropImageByRect();
